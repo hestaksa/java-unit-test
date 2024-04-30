9b30105aa5f873483ecb638b71f5ff8b3e0b4182
hestaksa <hestaksa@yandex.ru>
2024-04-30 17:17:54 +0300

    текст поменян

--- task_1.java
+++ task_1.java
@@ -3,5 +3,5 @@ public void checkIsAdultWhenAgeIsMoreThan18True() {
     Program program = new Program();
     boolean isAdult = program.checkIsAdult(17);
 
-    assertEquals("ОШИБКААААА",true, isAdult );// Напиши код здесь
+    assertEquals("ОШИБКААААА",true, isAdult );// Напиши код здесь  текст
 }
