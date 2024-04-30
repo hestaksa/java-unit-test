2de4cb4157f69fac579b5b49d99a3b18da28313a
hestaksa <hestaksa@yandex.ru>
2024-04-30 17:02:40 +0300

    Добавления 1

--- task_1.java
+++ task_1.java
@@ -1,1 +1,7 @@
-uyiyuiyuiyi   fhgghfg
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(17);
+
+    assertEquals("ОШИБКААААА",true, isAdult );// Напиши код здесь
+}
